88e4bae7fd7d1ddbc9c85b16702d348fc6eb33de
cleancodernz <cleancode@hotmail.com>
2024-10-04 23:26:45 +1300

    test

--- function_app.py
+++ function_app.py
@@ -8,7 +8,7 @@ app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 @app.route(route="spotifyintegration")
 
 #####################
-# main spotify integration
+# main spotify integration.
 #####################
 def spotifyintegration(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
